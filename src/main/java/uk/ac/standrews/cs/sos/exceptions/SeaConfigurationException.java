882a4031b61d0c2e53a96040d2cbaf56b32d4d8b
Simone Conte <sic2@st-andrews.ac.uk>
2016-05-03 15:36:07 +0100

    remove .class and .html files

--- src/main/java/uk/ac/standrews/cs/sos/exceptions/SeaConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.standrews.cs.sos.exceptions;
-
-/**
- * @author Simone I. Conte "sic2@st-andrews.ac.uk"
- */
-public class SeaConfigurationException extends Exception {
-}
--- src/main/java/uk/ac/standrews/cs/sos/exceptions/manifest/UnknownManifestTypeException.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.standrews.cs.sos.exceptions.manifest;
-
-/**
- * @author Simone I. Conte "sic2@st-andrews.ac.uk"
- */
-public class UnknownManifestTypeException extends Exception {
-}
