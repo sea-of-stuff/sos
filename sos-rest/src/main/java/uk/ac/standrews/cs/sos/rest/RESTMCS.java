568c80a327ba31a550712f758aa43eff1b590be4
sic2 <sic2@st-andrews.ac.uk>
2016-12-14 16:39:39 +0000

    MCS REST Filter and Binding

--- sos-rest/src/main/java/uk/ac/standrews/cs/sos/rest/RESTMCS.java
+++ sos-rest/src/main/java/uk/ac/standrews/cs/sos/rest/RESTMCS.java
@@ -1,7 +1,21 @@
 package uk.ac.standrews.cs.sos.rest;
 
+import uk.ac.standrews.cs.sos.bindings.MCSNode;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
 /**
  * @author Simone I. Conte "sic2@st-andrews.ac.uk"
  */
+@Path("/mcs/")
+@MCSNode
 public class RESTMCS {
+
+    @GET
+    @Path("/")
+    public Response test() {
+        return Response.status(200).entity("MCS-TEST").build();
+    }
 }
