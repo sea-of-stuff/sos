0d97acd5147a8306a1acf0e0f04186362572e472
sic2 <sic2@st-andrews.ac.uk>
2017-01-24 15:05:52 +0000

    PDF closure

---
+++ sos-core/src/main/java/uk/ac/standrews/cs/sos/model/context/closures/PDFClosure.java
@@ -0,0 +1,32 @@
+package uk.ac.standrews.cs.sos.model.context.closures;
+
+import uk.ac.standrews.cs.sos.actors.SOSAgent;
+import uk.ac.standrews.cs.sos.exceptions.metadata.MetadataNotFoundException;
+import uk.ac.standrews.cs.sos.interfaces.manifests.Asset;
+import uk.ac.standrews.cs.sos.interfaces.metadata.SOSMetadata;
+
+import java.util.function.Predicate;
+
+
+/**
+ * @author Simone I. Conte "sic2@st-andrews.ac.uk"
+ */
+public class PDFClosure extends BaseClosure {
+
+    private PDFClosure(SOSAgent agent, Predicate<Asset> predicate) {
+        super(agent, predicate);
+    }
+
+    public boolean test(Asset asset) {
+
+        try {
+            SOSMetadata metadata = agent.getMetadata(asset.getMetadata());
+            String contentType = metadata.getProperty("Content-Type");
+            return contentType.toLowerCase().equals("application/pdf");
+        } catch (MetadataNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
