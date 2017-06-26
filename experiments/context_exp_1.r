40e8a2dd7ca5843d31f3e5384bf4a5191e59aa4c
sic2 <sic2@st-andrews.ac.uk>
2017-06-26 12:45:02 +0100

    +

--- experiments/context_exp_1.r
+++ experiments/context_exp_1.r
@@ -1,61 +1,62 @@
 # This script reads a CSV file with the format:
 #
-# NoContexts, Time to run the context (must be a number)
-# 1, 10
-# 1, 10.4
-# 1, 9.88
-# 2, 15.3
-# 2, 16.8
-# .., ..
+# Configuration,Data Processed (MB),Time (s),Space Used (MB)
+# 1,100,2,1
+# 2,100,2.2,1.2
+# 3,100,1.9,0.9
+# 4,100,2.4,1.1
+# .., .., .., ..
 #
+# FIX COMMENT
 # The script then plots the average points per NoContexts.
 # Error bars are plotted using the standard deviation.
 #
 # Author: Simone Ivan Conte (sic2)
+#
+# Useful links:
+# https://datascienceplus.com/building-barplots-with-error-bars/
 
-setwd("/Users/sic2/Desktop/sos-experiments")
+setwd("/Users/sic2/git/sos/experiments")
 getwd()
 
 # Read the CVS file
 d <- read.csv("context_exp_1.csv", header=TRUE, sep=",")
 
-# Do some calculations on the data (d[,2])
-c_mean <- aggregate(d[,2], list(d[,1]), mean);
-c_mean <- setNames(c_mean, c(colnames(d[1]), 'mean'))
+d_throughput <- d[,2] / d[,3]
+aggr <- aggregate(d_throughput,
+                  by = list(config = d[, 1]),
+                  FUN = function(x) c(mean = mean(x), sd = sd(x), n = length(x)))
 
-c_sd <- aggregate(d[,2], list(d[,1]), sd);
-c_sd <- setNames(c_sd, c(colnames(d[1]), 'sd'))
+d_processed <- do.call(data.frame, aggr)
 
-c <- merge(c_mean, c_sd)
+# Compute standard error per group
+d_processed$se <- d_processed$x.sd / sqrt(d_processed$x.n)
 
-x <- c[,1]
-avg <- c$mean
-sdev <- c$sd
-
-phc_symbol <- 19
-
-plot(x, avg, 
-     type="p", pch=phc_symbol, # The phc param specified the symbol used for the data point
-     xlab="No Contexts", xaxt="n", # Hide the ticks (see the call to axis())
-     ylab="Cost (seconds)", 
-     ylim=range(c(0, avg+sdev)), # yAxis starts from 0. Alternatively c(avg-sdev, avg+sdev)
-     main="# Contexts vs Cost")
-
-# Error bars using standard deviation
-arrows(x, avg-sdev, x, avg+sdev, length=0.05, angle=90, code=3)
-
-# Draw x-axis with integers only
-xaxisInts <- seq(from=min(x), to=max(x), by=1)
-axis(1, at=xaxisInts)
-
-# Print a legend for the graph
-legend("topleft", c("Cost"), col=c("black"), pch=phc_symbol)
-
-#
-# The text below will be used as a caption when adding the plot to a document.
-# Caption: 
-# CAPTION_TEXT
+# Rename columns
+colnames(d_processed) <- c("Config", "mean", "sd", "n", "se")
+d_processed$names <- c(paste("Config", d_processed$Config))
 
 
-# STATISTICAL TESTS
-summary(d)
+# Estimate top limit on the y-axis
+plotTop <- max(d_processed$mean) + max(d_processed$se) +
+  d_processed[d_processed$mean == max(d_processed$mean), 5] * 5
+
+par(ask=F) # Do not ask to print plot on the console
+barCenters <- barplot(height = d_processed$mean,
+                    names.arg = d_processed$names,
+                    beside = true, las =2,
+                    ylim = c(0, plotTop),
+                    main = "Context throughput per different\n types of context configurations",
+                    ylab = "Throughput (MB/s)",
+                    border = "black",
+                    axes = T)
+
+# Add error bars
+
+segments(barCenters, d_processed$mean - d_processed$se * 2, barCenters,
+         d_processed$mean + d_processed$se * 2, lwd = 1.5)
+
+arrows(barCenters, d_processed$mean - d_processed$se * 2, barCenters,
+       d_processed$mean + d_processed$se * 2, lwd = 1.5, angle = 90,
+       code = 3, length = 0.05)
+
