64ecefb25e193a4300badc62edd5a32419081d51
sic2 <sic2@st-andrews.ac.uk>
2017-08-21 18:23:49 +0100

    R script to analyse PR_1 results

---
+++ experiments/context_exp_pr_1_working.R
@@ -0,0 +1,53 @@
+setwd("/Users/sic2/git/sos/experiments")
+getwd()
+
+# Read the CVS file
+d <- read.csv("output/pr_1__2017_08_21T15_35_33_894Z.TSV", header=TRUE, sep="\t")
+d$ContextName <- sapply(strsplit(as.character(d$Message), 'SHA'), '[', 1)
+
+a <- data.frame(V1=c(d$ContextName),
+                V2=c(d$User.Measure))
+
+aggr <- aggregate(d$User.Measure ~ d$ContextName,
+                  FUN = function(x) c(mean = mean(x), sd = sd(x), n = length(x)))
+
+d_processed <- do.call(data.frame, aggr)
+
+# Compute standard error per group
+d_processed$se <- d_processed[,3] / sqrt(d_processed[,4])
+
+# Rename columns
+colnames(d_processed) <- c("Configuration", "mean", "sd", "n", "se")
+d_processed$names <- d_processed$Config
+
+# Estimate top limit on the y-axis
+plotTop <- max(d_processed$mean) + max(d_processed$se) +
+  d_processed[d_processed$mean == max(d_processed$mean), 5] * 5
+
+par(ask=F) # Do not ask to print plot on the console
+par(mar=c(12,4,4,2)+3) # Add space to show all labels
+barCenters <- barplot(height = d_processed$mean,
+                      names.arg = d_processed$names,
+                      beside = true, las =2,
+                      ylim = c(0, plotTop),
+                      main = "Predicate performance against different settings",
+                      ylab = "Time (s)",
+                      border = "black",
+                      axes = T)
+
+# Add error bars
+segments(barCenters, d_processed$mean - d_processed$se * 2, barCenters,
+         d_processed$mean + d_processed$se * 2, lwd = 1.5)
+
+arrows(barCenters, d_processed$mean - d_processed$se * 2, barCenters,
+       d_processed$mean + d_processed$se * 2, lwd = 1.5, angle = 90,
+       code = 3, length = 0.05)
+
+
+# BOPLOT
+par(mar=c(12,4,4,2)+3) # Add space to show all labels
+x <- boxplot(d$User.Measure~d$ContextName, data=d,
+             las=2, # Draw x labels vertically
+             main="Predicate performance against different settings",
+             ylab="Time (s)")
+
