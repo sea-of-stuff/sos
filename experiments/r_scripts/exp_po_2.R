152f6fb58c39e8c705b30388026662f9febaae6b
sic2 <sic2@st-andrews.ac.uk>
2017-11-06 15:10:53 +0000

    PO_2 configuration + po_2 r script

--- experiments/r_scripts/exp_po_2.R
+++ experiments/r_scripts/exp_po_2.R
@@ -14,10 +14,17 @@ setwd("/Users/sic2/git/sos/experiments")
 getwd()
 
 # Read the CVS file
-d <- read.csv("output/po_1__2017_10_31T12_43_49_502Z.tsv", header=TRUE, sep="\t")
+d <- read.csv("output/po_2__2017_11_06T14_58_41_690Z.tsv", header=TRUE, sep="\t")
 d <- d[d$StatsTYPE == 'policies',] # Filter policies measurements
 d$Message <- droplevels(d$Message)
 d$ContextName <- d$Message
+
+d$ContextName<-factor(d$ContextName, levels=c("no_policies", 
+                                              "one_policy_local", "two_policies_local", "three_policies_local",
+                                              "four_policies_local", "five_policies_local", "six_policies_local",
+                                              "seven_policies_local", "eight_policies_local", "nine_policies_local", "ten_policies_local"
+))
+
 
 d$Measures <- d$User.Measure / 1000000000.0; # Nanoseconds to seconds
 
@@ -35,29 +42,15 @@ d_processed$names <- d_processed$Config
 
 colors = c(rep("red",1),rep("deepskyblue",2),rep("green",1))
 
-##################
-# BOXPLOT
-par(mar=c(10,4,4,2)+3) # Add space to show all labels
-x <- boxplot(d$Measures~d$ContextName, data=d,
-             outline=FALSE,
-             las=2, # Draw x labels vertically
-             ylab="Time (s) - log scale", 
-             xlab="Policies",
-             log = "y",
-             col=colors)
-
-
-legend("topright", legend=c("Remote", "Local", "Base"),
-       fill=c("red", "deepskyblue", "green"), cex=0.8, inset=.05)
-
 
 ################
 # Playing with ggplot
 
 ggplot(data=d, aes(x=d$ContextName, y=d$Measures)) + 
-  geom_boxplot(outlier.alpha = 0.1) +
-  geom_point(color="tomato", position="jitter", alpha=.05) +
+  geom_boxplot(outlier.alpha = 1) +
+  geom_point(color="tomato", position="jitter", alpha=.5) +
   geom_rug(side="1") +
+  theme_bw() +
   theme(axis.text.x=element_text(angle=90,hjust=1), 
         axis.text=element_text(size=14),
         axis.title=element_text(size=16,face="bold")) +
@@ -66,6 +59,6 @@ ggplot(data=d, aes(x=d$ContextName, y=d$Measures)) +
 
 ggplot(data=d, aes(x=d$ContextName, y=d$Measures)) + 
   geom_bar(position=position_dodge(), stat="identity", width=.5) +
-  geom_point(color="tomato", position="jitter", alpha=.05) +
+  geom_point(color="tomato", position="jitter", alpha=.5) +
   geom_rug(side="1") +
   labs(title="Policies per asset....", x="Policy", y="Time (s)")
