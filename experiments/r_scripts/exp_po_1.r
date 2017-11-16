8a296e628bf0bb6b37ebc45af3884c8f5668eec1
sic2 <sic2@st-andrews.ac.uk>
2017-11-16 19:15:26 +0000

    r script for po_a_1

--- experiments/r_scripts/exp_po_1.r
+++ experiments/r_scripts/exp_po_1.r
@@ -14,7 +14,7 @@ setwd("/Users/sic2/git/sos/experiments")
 getwd()
 
 # Read the CVS file
-d <- read.csv("output/po_1__2017_10_31T12_43_49_502Z.tsv", header=TRUE, sep="\t")
+d <- read.csv("remote/po_a_1_100kb_its_5.tsv", header=TRUE, sep="\t")
 d <- d[d$StatsTYPE == 'policies',] # Filter policies measurements
 d$Message <- droplevels(d$Message)
 d$ContextName <- d$Message
@@ -24,18 +24,17 @@ d$Measures <- d$User.Measure / 1000000000.0; # Nanoseconds to seconds
 ################
 # Playing with ggplot
 
-ggplot(data=d, aes(x=d$ContextName, y=d$Measures)) + 
-  geom_boxplot(outlier.alpha = 0.1) +
-  geom_point(color="tomato", position="jitter", alpha=.05) +
-  geom_rug(sides="l") +
+
+dd <- d[d$Subtype == 'policy_apply_dataset',]
+dd$Message <- droplevels(dd$Message)
+dd <- summarySE(dd, measurevar="Measures", groupvars =c("ContextName", "StatsTYPE"))
+
+ggplot(data=dd, aes(x=dd$ContextName, y=dd$Measures)) + 
+  geom_point() +
+  geom_errorbar(aes(ymin=dd$Measures-dd$ci, ymax=dd$Measures+dd$ci),width=.2) +
+  theme_bw() +
   theme(axis.text.x=element_text(angle=90,hjust=1), 
         axis.text=element_text(size=14),
         axis.title=element_text(size=16,face="bold")) +
   labs(title="Policies per asset....", x="Policy", y="Time (s)")
 
-
-ggplot(data=d, aes(x=d$ContextName, y=d$Measures)) + 
-  geom_bar(position=position_dodge(), stat="identity", width=.5) +
-  geom_point(color="tomato", position="jitter", alpha=.05) +
-  geom_rug(sides="l") +
-  labs(title="Policies per asset....", x="Policy", y="Time (s)")
