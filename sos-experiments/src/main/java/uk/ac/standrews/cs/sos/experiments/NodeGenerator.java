0efb34cac458d85dfe073088d61bdc4aa0071265
sic2 <sic2@st-andrews.ac.uk>
2017-12-12 18:13:02 +0000

    Better node generator utility + nodes for co_a_1

--- sos-experiments/src/main/java/uk/ac/standrews/cs/sos/experiments/NodeGenerator.java
+++ sos-experiments/src/main/java/uk/ac/standrews/cs/sos/experiments/NodeGenerator.java
@@ -1,8 +1,13 @@
 package uk.ac.standrews.cs.sos.experiments;
 
+import org.bouncycastle.util.encoders.Base64;
+import uk.ac.standrews.cs.guid.GUIDFactory;
+import uk.ac.standrews.cs.guid.IGUID;
+import uk.ac.standrews.cs.guid.exceptions.GUIDGenerationException;
 import uk.ac.standrews.cs.utilities.crypto.CryptoException;
 import uk.ac.standrews.cs.utilities.crypto.DigitalSignature;
 
+import java.io.ByteArrayInputStream;
 import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.util.Scanner;
@@ -12,7 +17,7 @@ import java.util.Scanner;
  */
 public class NodeGenerator {
 
-    public static void main(String[] args) throws CryptoException {
+    public static void main(String[] args) throws CryptoException, GUIDGenerationException {
 
         String path = "sos-experiments/src/main/resources/generated_nodes/";
 
@@ -22,7 +27,15 @@ public class NodeGenerator {
 
         KeyPair keys = DigitalSignature.generateKeys();
         DigitalSignature.persist(keys, Paths.get(path + nodeName), Paths.get(path + nodeName));
+        System.out.println("-------------------");
+        System.out.println("Generated certificate and key for node " + nodeName + " at base path " + path + nodeName);
+        System.out.println("One-line certificate to use in bootstrap node setting: " + new String(Base64.encode((keys.getPublic().getEncoded()))));
 
-        System.out.println("Generate certificate and key for node " + nodeName + " at base path " + path + nodeName);
+        IGUID guid = GUIDFactory.generateGUID(new ByteArrayInputStream(keys.getPublic().getEncoded()));
+        System.out.println("-------------------");
+        System.out.println("GUID of node is: " + guid.toMultiHash());
+
+        System.out.println("-------------------");
+        System.out.println("Find node ip by running 'ifconfig' and checking the inet IP address for the em1 interface (hogun cluster)");
     }
 }
