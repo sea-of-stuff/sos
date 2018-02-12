b06bd2fe48ca3dfcd485b84f1dda37c4dcb51ecd
sic2 <simone.ivan.conte@gmail.com>
2018-02-12 11:01:34 +0000

    Failure_7 - needs testing

---
+++ sos-experiments/src/main/java/uk/ac/standrews/cs/sos/experiments/experiments/Experiment_Failure_7.java
@@ -0,0 +1,83 @@
+package uk.ac.standrews.cs.sos.experiments.experiments;
+
+import uk.ac.standrews.cs.sos.experiments.Experiment;
+import uk.ac.standrews.cs.sos.experiments.ExperimentConfiguration;
+import uk.ac.standrews.cs.sos.experiments.ExperimentUnit;
+import uk.ac.standrews.cs.sos.experiments.exceptions.ExperimentException;
+import uk.ac.standrews.cs.sos.instrument.InstrumentFactory;
+import uk.ac.standrews.cs.sos.instrument.StatsTYPE;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Simulate failure by making remote nodes unresponsive to REST calls (except a special one that is needed to wake the node up again)
+ *
+ *
+ * @author Simone I. Conte "sic2@st-andrews.ac.uk"
+ */
+public class Experiment_Failure_7 extends Experiment_Failure implements Experiment {
+
+    public Experiment_Failure_7(ExperimentConfiguration experimentConfiguration, String outputFilename) throws ExperimentException {
+        super(experimentConfiguration, outputFilename);
+
+        List<ExperimentUnit> units = new LinkedList<>();
+        for(int i = 0; i < experiment.getSetup().getIterations(); i++) {
+            units.add(new ExperimentUnit_Failure_5(i));
+        }
+        Collections.shuffle(units);
+
+        experimentUnitIterator = units.iterator();
+    }
+
+    private class ExperimentUnit_Failure_5 extends ExperimentUnit_Failure {
+
+        ExperimentUnit_Failure_5(int index) {
+            super(index);
+        }
+
+        @Override
+        public void run() throws ExperimentException {
+            InstrumentFactory.instance().measure(StatsTYPE.experiment, StatsTYPE.experiment, "Experiment Unit Index", System.nanoTime(), index);
+
+            System.out.println("Running Policies");
+            cms.runPolicies();
+            cms.runCheckPolicies();
+
+            // Disable REST API on remote nodes
+            Runnable task = () -> {
+                try {
+                    changeRESTAPIonAllNodes(0, true);
+                } catch (ExperimentException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            Thread thread = new Thread(task);
+            thread.start();
+
+
+            // The check policy thread runs every 30 seconds according to the master experiment node configuration (see sif_12.json).
+            rest_a_bit(90 * 1000); // 1.5 minutes
+
+            // Re-Disable REST API on remote nodes
+            task = () -> {
+                try {
+                    changeRESTAPIonAllNodes(15, false);
+                } catch (ExperimentException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            thread = new Thread(task);
+            thread.start();
+
+            // The check policy thread runs every 30 seconds according to the master experiment node configuration (see sif_12.json).
+            rest_a_bit(180 * 1000); // 3 minutes
+
+            writePolicyCheckStats();
+        }
+
+    }
+}
---
+++ sos-experiments/src/main/resources/experiments/failure_7/configuration/README.md
@@ -0,0 +1,43 @@
+# Removed temporarily node sif-8
+
+## from configuration-sif
+
+{
+    "id" : 8,
+    "name" : "sif8",
+    "path" : "/cs/scratch/",
+    "remote" : true,
+    "java" : "/usr/local/jdk/bin/java",
+    "ssh" : {
+      "type" : 1,
+      "host" : "sif-8.cluster",
+      "user" : "sic2",
+      "known_hosts": "/Users/sic2/.ssh/known_hosts",
+      "config": "/Users/sic2/.ssh/config",
+      "privatekeypath": "/Users/sic2/.ssh/id_rsa",
+      "passphrase" : "85GDkVfAMHAV//2ZcoTeW8YzfB1mQHdbm9A/ZdSYuRY6Va2whCYprV0Uh+Dx+ZxGnAmK5SRIzFIfPjrqyeHzYA=="
+    },
+    "configurationfile" : "sif_8.json",
+    "certificatefile" : "sif_8.crt",
+    "keyfile" : "sif_8.key",
+    "behaviour": {
+      "start": 0,
+      "stop": -1,
+      "pause": [],
+      "operations": [ "WIP" ]
+    }
+}
+
+## from sif-12
+
+{
+    "guid" : "SHA256_16_e56ed40c6857d2aba85d9e06a2044d845c851b5456772d60402a6d8049f80086",
+    "hostname" : "192.168.1.8",
+    "port" : 8080,
+    "certificate" : "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAJqrKs2NDoGiC3OV/wiLG1DrK32rUB1PTdIcdOXbzQe4BkUXDlkIfCNwbK8B7N94zb0RYOpqcPsJdGfHJEYGVrkCAwEAAQ=="
+}
+
+
+## from context
+
+"SHA256_16_e56ed40c6857d2aba85d9e06a2044d845c851b5456772d60402a6d8049f80086"
